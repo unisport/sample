bcaf55e314d2bd5014bec3e808ed4001d5af1b46
stefanhaupt <stefanhaupt@mac.com>
2014-01-06 16:47:21 +0100

    Create Product.py

---
+++ Product.py
@@ -0,0 +1,14 @@
+class Product(object):
+    name = ""
+    kids = 0
+    sizes = ""
+    url = ""
+    free_porto = 0
+    price = 0
+    package = ""
+    delivery = ""
+    kid_adult = 0
+    price_old = 0
+    img_url = ""
+    id = 0
+    women = 0
