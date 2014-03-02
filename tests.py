bbd4cccc1360ba32774c0ca881e995f999d10514
Finnur Torfason <finnurtorfa@gmail.com>
2014-03-02 20:07:29 +0100

    Add a testing skeleton

--- tests.py
+++ tests.py
@@ -1,1 +1,34 @@
-import unittest
+#!/usr/bin/env python
+
+import os, unittest, shutil
+
+import sample
+
+class SampleTestCase(unittest.TestCase):
+  def setUp(self):
+    self.basedir = os.path.split(os.path.abspath(__file__))[0]
+    os.chdir(self.basedir)
+
+    sample.app.config['TESTING'] = True
+
+    try:
+      os.remove('/tmp/sample.db')
+      shutil.rmtree(self.basedir + '/test_migrations')
+    except OSError:
+      pass
+
+    os.system("python manage.py db init --directory=test_migrations")
+    os.system("python manage.py db migrate --directory=test_migrations")
+    os.system("python manage.py db upgrade --directory=test_migrations")
+    os.system("python manage.py seed")
+
+  def tearDown(self):
+    try:
+      os.remove('/tmp/sample.db')
+      shutil.rmtree(self.basedir + '/test_migrations')
+    except OSError:
+      pass
+
+if __name__ == '__main__':
+  unittest.main()
+
