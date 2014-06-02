922a03b4323fa24f4ecc914cee3e5629cc368545
Kasper <brandt265@gmail.com>
2014-06-02 10:27:52 +0200

    update

--- manage.py
+++ manage.py
@@ -3,11 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unisport.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unibet.settings")
->>>>>>> 11a138d8bd8f3db99059a41adc16b714ed3293cf
+
 
     from django.core.management import execute_from_command_line
 
