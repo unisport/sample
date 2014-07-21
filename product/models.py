d514924e31dee59f23ec934c6b2a59ebeeabf672
Wenbin Wu <admin@wenbinwu.com>
2014-07-21 11:43:18 +0200

    init product app

---
+++ product/models.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from django.db import models
+
+
+class Product(models.Model):
+
+    p_id = models.IntegerField()
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=16, decimal_places=2)
+    price_old = models.DecimalField(max_digits=16, decimal_places=2)
+    img_url = models.URLField()
+    url = models.URLField()
+
+    package = models.IntegerField()
+    min_delivery_day = models.IntegerField()
+    max_delivery_day = models.IntegerField()
+
+    free_porto = models.BooleanField()
+    kid_adult = models.IntegerField()
+    kids = models.IntegerField()
+    women = models.IntegerField()
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=128)
---
+++ product/views.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+from django.shortcuts import render
+
+
+def products(request):
+    pass
+
+
+def single_products(request):
+    pass
+
+
+def kids(request):
+    pass
