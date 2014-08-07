4dcb6ac261415e0ff4bc982460b9fe115b14c774
goldbach <jakob.goldbach@gmail.com>
2014-08-08 00:26:32 +0200

    pylint cleanup

--- app1.py
+++ app1.py
@@ -1,4 +1,4 @@
-""" First shot at Flask -- seems less boilerplate than Django 
+""" First shot at Flask -- seems less boilerplate than Django
 
 Notes:
 
@@ -7,56 +7,61 @@ debug=True is ok in non-prod environments
 
 Started out with products() that did the sorting and returned 10 items
 
-Then kids, and sorting and returning any number of product was made in get_items_by_price 
-and products() changed also
+Then kids, and sorting and returning any number of product
+was made in get_items_by_price and products() changed also
 
-Then I did the product by id, but first turned data structure into dict. It was trivial to change get_items_by_price
-and products() and kids() did not have to be changed
+Then I did the product by id, but first turned data structure into dict.
+It was trivial to change get_items_by_price and products() and kids()
+did not have to be changed.
 
-For the pagination I first implemented the optional offset in get_items_by_price. Then changed products() to support
-?page and ?count
+For the pagination I first implemented the optional offset in
+get_items_by_price(). Then changed products() to support ?page and ?count.
 
-I have no experience in testing web apps so this Flask app is without tests for now. Obviously wrong!
+I have no experience in testing web apps so this Flask app is without tests
+for now. Obviously wrong!
 
 
 """
 
 import flask
-import db
 import json
 
 # our dict of products.
-data = db.data
+from db import DATA
 
 app = flask.Flask(__name__)
 
 # workhorse of this app: get n elements at any offset ordered by price
-def get_items_by_price(n=None, offset=0):
-    sort_prod = sorted(data.values(), key=lambda x: x['price'])
-    if n:
-        return sort_prod[offset:offset+n]
+def get_items_by_price(n_items=None, offset=0):
+    """ get n_items elemenent at any offset """
+    sort_prod = sorted(DATA.values(), key=lambda x: x['price'])
+    if n_items:
+        return sort_prod[offset:offset+n_items]
     else:
         return sort_prod
 
 # also supports /products/?count=20&page=4
 @app.route('/products/')
 def products():
+    """ return products, supports any number and offset """
     # users count from 1
-    page  = int(flask.request.args.get('page', '1'))
+    page = int(flask.request.args.get('page', '1'))
     # computer scientists from 0
     page -= 1
-    count  = int(flask.request.args.get('count', '10'))
+    count = int(flask.request.args.get('count', '10'))
     # this is wrong if we add/remove products while user is paginating
     return json.dumps(get_items_by_price(count, count*page))
 
 @app.route('/products/kids')
 def kids():
-    return json.dumps(filter(lambda x: x['kids'], get_items_by_price() ))
+    """ return kids stuff """
+    return json.dumps(filter(lambda x: x['kids'], get_items_by_price()))
 
 @app.route('/products/<int:pid>/')
 def by_id(pid):
+    """ lookup by product id """
     # in list to be consistent with other methods
-    return json.dumps([ data.get(pid, "No such product id %d" % pid) ] )
+    return json.dumps([DATA.get(pid, "No such product id %d" % pid)])
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,16 +1,20 @@
 """ Load data and cast values to Python types
 
-data will a dict with product id as key and product as value. Each product is a dict with values cast to meaningful types
+data will a dict with product id as key and product as value.
+Each product is a dict with values cast to meaningful types
 
 Notes:
 
-First version of our main data structure was a list of products as this it how it came from sample api.
+First version of our main data structure was a list of products
+as this it how it came from sample api.
 
-I realized I had to casting to do when I saw prices was in DK locale. I wrote a simple testcase under __main__ which
-I find useful as a minimal testing "framework". I decided to cast a few others to bool (kids in particular) and then product id to int
+I realized I had to casting to do when I saw prices was in DK locale.
+I wrote a simple testcase under __main__ which I find useful as a minimal
+testing "framework". Decided to cast a few others to bool (kids in particular)
+and then product id to int.
 
-When I had to get product by id I changed into a dict of products with id as key (which I should have done up front)
-
+When I had to get product by id I changed into a dict of products
+with id as key (which I should have done up front)
 
 """
 
@@ -20,52 +24,50 @@ import urllib2
 import json
 
 
-def to_float(s):
-    s = s.replace(',', '.')
-    return float(s)
+def to_float(val):
+    """ dk locale to python - breaks with thousand seperator"""
+    return float(val.replace(',', '.'))
 
-def to_bool(s):
-    if s == u'1' or s == u'True':
+def to_bool(val):
+    """ json string to py bool """
+    if val == u'1' or val == u'True':
         return True
     else:
         return False
 
 # function map for casting to types
-conv = {'price':      to_float,
+CONV = {'price':      to_float,
         'price_old':  to_float,
         'id':         int,
         'package':    to_bool,
         'free_porto': to_bool,
         'kids':       to_bool,
         'women':      to_bool,
-        'kid_adult':  to_bool,
-}
+        'kid_adult':  to_bool, }
 
-data_pre = json.loads(urllib2.urlopen('http://unisport.dk/api/sample').read())['latest']
+DATA_PRE = json.loads(
+    urllib2.urlopen('http://unisport.dk/api/sample').read())['latest']
 # our in-memory "backend" database
-data = {}
+DATA = {}
 # cast keys to right type and save under product id
-for o in data_pre:
+for o in DATA_PRE:
     for k in o.keys():
-        f = conv.get(k, None)
+        f = CONV.get(k, None)
         if f:
             o[k] = f(o[k])
-    data[o['id']]= o
+    DATA[o['id']] = o
 
 
 if __name__ == "__main__":
-    
     # minimal testing
-
     assert to_bool('2') == False
     assert to_bool('1') == True
     assert to_bool('0') == False
     assert to_float('346.45') == 346.45
     assert to_float('30') == 30.0
 
-    product  = data.values()[0]
-    assert isinstance(product['id'], int )
-    assert isinstance(product['price'], float )
-    assert isinstance(product['price'], float )
-    
+    product = DATA.values()[0]
+    assert isinstance(product['id'], int)
+    assert isinstance(product['price'], float)
+    assert isinstance(product['price'], float)
     assert product['price'] == 149.00
