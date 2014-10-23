2683fcb7396280a4ca6e8246ab88e9df286098ab
Sergey Smirnov <smirnoffs@gmail.com>
2014-10-23 21:05:27 +0300

    Change Boolean to CharField in Product model. Why everything is in strings not boolean or integers?

--- products/models.py
+++ products/models.py
@@ -20,12 +20,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=16, decimal_places=2, default=0)
     price_old = models.DecimalField(max_digits=16, decimal_places=2, default=0)
 
-    package = models.IntegerField(default=0)
-    delivery = models.IntegerField(default=0)
-    free_porto = models.IntegerField(default=0)
-    kids = models.IntegerField(default=0)
-    kid_adult = models.IntegerField(default=0)
-    women = models.IntegerField(default=0)
+    package = models.CharField(max_length=1, default="0")
+    delivery = models.CharField(max_length=1, default=0)
+    free_porto = models.CharField(max_length=16, default="False")
+    kids = models.CharField(max_length=1, default="0")
+    kid_adult = models.CharField(max_length=1, default=0)
+    women = models.CharField(max_length=1, default=0)
 
     sizes = models.ManyToManyField(Size)
 
