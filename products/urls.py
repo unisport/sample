bd7f73e82b41b5f241d4fd953f0743161ef58d6f
Sergey Smirnov <smirnoffs@gmail.com>
2014-10-23 21:52:04 +0300

    name for import_data

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, url
 __author__ = 'Sergey Smirnov <smirnoffs@gmail.com>'
 
 urlpatterns = patterns('products',
-                       url(r'^$', 'views.products', name='catalog'),
-                       url(r'^kids/$', 'views.kids', name='kids'),
+                       url(r'^$', 'views.products'),
+                       url(r'^kids/$', 'views.kids'),
                        url(r'^(?P<id>\d+)/$', 'views.product'),
+                       url(r'import_data/$', 'views.import_data', name='import_data'),
                        )
 
