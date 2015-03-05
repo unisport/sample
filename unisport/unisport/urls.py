78febdb2316676d9c7f0af6b07082ceed0dbec91
Silviu Tantos <me@razius.com>
2015-03-05 17:18:34 +0100

    Initial project skeleton.

---
+++ unisport/unisport/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+)
