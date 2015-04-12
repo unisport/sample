40502350839862f7dafbcec40fb377457072b5f3
Andrey Sokolov <a.sokolov@venista.com>
2015-04-12 14:20:10 +0300

    Update unittests for products api

--- source/unisample/api/product/tests/test_product_service.py
+++ source/unisample/api/product/tests/test_product_service.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
+from unisample.api.product.plain_models import ProductData
 from unisample.api.product.services.product_service import ProductService
 from .utils import create_product
+
+
+class UserMock(object):
+    pass
 
 
 class TestProductService(TestCase):
@@ -33,3 +38,52 @@ class TestProductService(TestCase):
         result = self.product_service.get_product_detail(product.pk)
 
         self.assertEqual(product, result)
+
+    def test_04_create_product(self):
+        user = UserMock()
+        product_data = self._get_test_product_data()
+
+        result = self.product_service.create_product(user, product_data)
+        self.assertEqual(result.name, product_data.name)
+        self.assertEqual(result.price, product_data.price)
+        self.assertEqual(result.price_old, product_data.price_old)
+
+    def test_05_update_product(self):
+        user = UserMock()
+        product = create_product('Product name')
+
+        product_data = self._get_test_product_data()
+        product_data.name = 'New product name'
+        product_data.pk = product.pk
+
+        self.product_service.update_product(user, product_data)
+        product = self.product_service.get_product_detail(product.pk)
+        self.assertEqual(product.name, product_data.name)
+
+    def test_06_delete_product(self):
+        user = UserMock()
+        product = create_product('Product name')
+
+        product = self.product_service.get_product_detail(product.pk)
+        self.assertIsNotNone(product)
+
+        self.product_service.delete_product(user, product.pk)
+
+        product = self.product_service.get_product_detail(product.pk)
+        self.assertIsNone(product)
+
+    def _get_test_product_data(self):
+        return ProductData(
+            name="Product Name",
+            price=209.0,
+            price_old=349.0,
+            kids=1,
+            women=0,
+            kid_adult=0,
+            delivery="1-2 dage",
+            free_porto=True,
+            package=0,
+            sizes="EU 27\u00bd/US 10\u00bdC",
+            img_url="http://s3-eu-west-1.amazonaws.com/product-img/121973_da_mellem.jpg",
+            url="http://www.unisport.dk/fodboldstoevler/nike-fc247-elastico-pro-iii-tf-orangesortneon-brn/121973/",
+        )
