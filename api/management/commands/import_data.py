bb44b54218ef2a74593b6cc44253b5b2e788f436
tomasz_anielak <tanielak@gmail.com>
2015-07-04 18:21:04 +0200

    added import_data command and tested it

---
+++ api/management/commands/import_data.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ValidationError
+from api.models import Product
+import urllib2
+import json
+
+# use this command to import data from the provided endpoint
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # get the provided json data
+        data_url = 'http://www.unisport.dk/api/sample/'
+        try:
+            page = urllib2.urlopen(data_url)
+        except Exception, e:
+            print e
+
+        content = page.read()
+        data = json.loads(content)
+
+        # import data to the database
+        for product in data['products']:
+            # change danish number notation to a pythonic representation, e.g.
+            # 1.099,00 becomes 1099.00
+            product.update(price=product['price'].replace('.', '').replace(',', '.'))
+            product.update(price_old=product['price_old'].replace('.', '').replace(',', '.'))
+
+            # perform model validation on the provided data before saving
+            # if there are no exception save the model
+            #
+            # full_clean() validates the model fields and uniqueness
+            # it also returns a clean value for each field by converting it to proper python type
+            # this means that, e.g.
+            # for BooelanField we can use 0, 1, True, False, 't', 'True', '1', 'f' 'False', '0'
+            try:
+                product = Product(**product)
+                product.full_clean()
+                product.save()
+            except ValidationError as e:
+                print e.message_dict
--- tests.py
+++ tests.py
@@ -1,1 +1,11 @@
 import unittest
+from django.test import TestCase
+from django.core.management import call_command
+from api.models import Product
+
+class APITests(TestCase):
+
+    def test_import_command(self):
+        call_command('import_data')
+        products = Product.objects.all().count()
+        self.assertEqual(products, 25)
