cdf4b044cda18ea86b49681d3d47bb401823b920
tomasz_anielak <tanielak@gmail.com>
2015-07-05 11:36:56 +0200

    Extended the service so new products can be added and tested it

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.core.paginator import Paginator, InvalidPage
-from unisport.settings import PAGINATION_SIZE
 from django.http import Http404, JsonResponse
+from django.core.exceptions import ValidationError
 
+from unisport.settings import PAGINATION_SIZE
 from api.models import Product
+
+import json
+
 class ProductList(View):
 
     def get(self, request):
@@ -23,6 +27,29 @@ class ProductList(View):
         serialized_data = [product.to_dict() for product in products]
 
         return JsonResponse(serialized_data, safe=False)
+
+    def post(self, request):
+
+        # check if format of the data is valid
+        try:
+            data = json.load(request)
+        except ValueError as e:
+            return JsonResponse({'status': 'Invalid format'}, status=400)
+
+        # validate the data
+        # no errors - save it and return successful response
+        # errors - return error response and indicate what is the problem
+        try:
+            product = Product(**data)
+            product.full_clean()
+            product.save()
+        except TypeError:
+            return JsonResponse({'status': 'Invalid attribute'}, status=400)
+        except ValidationError as e:
+            return JsonResponse({'status': 'Validation errors', 'errors': e.message_dict}, status=400)
+
+        return JsonResponse({'status': 'Created', 'product': product.to_dict()}, status=201)
+
 
 class ProductKids(View):
     def get(self, request):
--- tests.py
+++ tests.py
@@ -80,3 +80,76 @@ class APITests(TestCase):
         # product not found
         response = self.client.get(self.detail_url % 2)
         self.assertEqual(response.status_code, 404)
+
+    def test_post(self):
+        # valid data
+        data = json.dumps({
+            "kids": "1",
+            "name": "Test",
+            "sizes": "One Size",
+            "kid_adult": "0",
+            "free_porto": "0",
+            "price": "0.0",
+            "package": "0",
+            "delivery": "1-2 dage",
+            "url": "http://www.unisport.dk/gavekort/",
+            "price_old": "0.0",
+            "img_url": "http://s3-eu-west-1.amazonaws.com/product-img/1_maxi_0.jpg",
+            "women": "0",
+        })
+        response = self.client.post(self.list_url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'Created')
+        self.assertEqual(content['product']['name'], 'Test')
+        test_product = Product.objects.filter(name='Test').count()
+        self.assertEqual(test_product, 1)
+
+        # invalid data format
+        response = self.client.post(self.list_url, '{fsadf/..}', content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'Invalid format')
+
+        # data with validation errors
+        data = json.dumps({
+            "kids": "1",
+            "name": "Test",
+            "sizes": "One Size",
+            "kid_adult": "0",
+            "free_porto": "0",
+            "price": "0.0",
+            "package": "0",
+            "delivery": "1-2 dage",
+            "price_old": "0.0",
+            "img_url": "http://s3-eu-west-1.amazonaws.com/product-img/1_maxi_0.jpg",
+            "women": "0",
+        })
+        response = self.client.post(self.list_url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'Validation errors')
+        self.assertEqual(content['errors']['url'][0], 'This field cannot be blank.')
+        self.assertEqual(content['errors']['name'][0], 'Product with this Name already exists.')
+
+        # data with invalid attribute
+        data = json.dumps({
+            "kids": "1",
+            "name": "Test2",
+            "description": "Test description",  # no such field on the Product model
+            "sizes": "One Size",
+            "kid_adult": "0",
+            "free_porto": "0",
+            "price": "0.0",
+            "package": "0",
+            "delivery": "1-2 dage",
+            "url": "http://www.unisport.dk/gavekort/",
+            "price_old": "0.0",
+            "img_url": "http://s3-eu-west-1.amazonaws.com/product-img/1_maxi_0.jpg",
+            "women": "0",
+        })
+
+        response = self.client.post(self.list_url, data, content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        content = json.loads(response.content)
+        self.assertEqual(content['status'], 'Invalid attribute')
