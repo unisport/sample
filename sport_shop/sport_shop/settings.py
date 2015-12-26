03d75d92f03486a007019aef14d1ba88e18c6ba0
Fredrik Kock <fredrik@falconsocial.com>
2015-12-26 12:40:34 +0100

    Use postgresdb.

--- sport_shop/sport_shop/settings.py
+++ sport_shop/sport_shop/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'sport_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sport_shop',
     }
 }
 
