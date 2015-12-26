646f73389249d78d155deb273d32405ea28a3e8f
Fredrik Kock <fredrik@falconsocial.com>
2015-12-26 12:42:24 +0100

    Products have name, price, size, etc.

--- sport_shop/products/models.py
+++ sport_shop/products/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    Description: Products with name, price, size etc.
+    """
+    #TODO Optimize char model max lenghts
+    #TODO Normalize (sizes, delivery, and prices in separate tables)
+    delivery = models.CharField(max_length=200)
+    free_porto = models.BooleanField()
+    img_url = models.CharField(max_length=500)
+    kid_adult = models.BooleanField()
+    kids = models.BooleanField()
+    name = models.CharField(max_length=500)
+    package = models.BooleanField()
+    price = models.FloatField()
+    price_old = models.FloatField()
+    sizes = models.CharField(max_length=500)
+    url = models.CharField(max_length=500)
+    women = models.BooleanField()
