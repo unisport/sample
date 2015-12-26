9fc3daf6595c1a68ecfd7c2137cb11bf1c5fcf0d
Fredrik Kock <fredrik@falconsocial.com>
2015-12-26 13:11:29 +0100

    Parse price and price_old values (convert punctuation and cast to float).

--- sport_shop/products/models.py
+++ sport_shop/products/models.py
@@ -2,12 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Product(models.Model):
     """
     Description: Products with name, price, size etc.
     """
-    #TODO Optimize char model max lenghts
-    #TODO Normalize (sizes, delivery, and prices in separate tables)
+    # TODO Optimize char model max lenghts
+    # TODO Normalize (sizes, delivery, and prices in separate tables)
     delivery = models.CharField(max_length=200)
     free_porto = models.BooleanField()
     img_url = models.CharField(max_length=500)
@@ -20,3 +21,9 @@ class Product(models.Model):
     sizes = models.CharField(max_length=500)
     url = models.CharField(max_length=500)
     women = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+        for attr in ('price', 'price_old'):
+            if isinstance(getattr(self, attr), basestring):
+                setattr(self, attr, float(getattr(self, attr).replace('.', '').replace(',', '.')))
+        super(Product, self).save(*args, **kwargs)
