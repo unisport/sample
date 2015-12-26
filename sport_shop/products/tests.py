fbcfa824112ea25f1b25a26ae3dcc284255b5ac1
Fredrik Kock <fredrik@falconsocial.com>
2015-12-26 13:10:05 +0100

    Make sure downloaded fixtures are successfully parsed and saved to database.

--- sport_shop/products/tests.py
+++ sport_shop/products/tests.py
@@ -2,8 +2,17 @@ from django.test import TestCase
 
 from management.commands import get_fixtures
 
+from products.models import Product
+
 
 class GetFixturesTest(TestCase):
+
     def test_json_format(self):
         fixtures = get_fixtures.download_json()
         self.assertIsInstance(fixtures, dict)
+
+    def test_saving_fixtures(self):
+        fixtures = get_fixtures.download_json()
+        product_data = fixtures.get('products')[0]
+        saved_product = get_fixtures.save_product(product_data)
+        self.assertIsInstance(saved_product, Product)
