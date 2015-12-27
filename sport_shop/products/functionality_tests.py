a80f3d7cce005108fc61171836137604c85819a5
Fredrik Kock <fredrik@falconsocial.com>
2015-12-27 21:04:32 +0100

    Added docstrings to all test methods.

--- sport_shop/products/functionality_tests.py
+++ sport_shop/products/functionality_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
-import requests
 from selenium import webdriver
+import requests
 
 PRODUCT_FIXTURE = {
     "kids": "0",
@@ -23,16 +23,25 @@ API_TOKEN = 'token'
 class ListProductsTest(unittest.TestCase):
 
     def setUp(self):
+        """Open product listing page.
+        """
         self.browser = webdriver.Firefox()
         self.browser.get('http://localhost:8000/products/')
 
     def tearDown(self):
+        """Close browser.
+        """
         self.browser.quit()
 
     def test_page_title(self):
+        """Confirm that page title indicates that all products are listed.
+        """
         self.assertEqual('Sport Shop - Products', self.browser.title)
 
     def test_thumbnail_count(self):
+        """Confirm that pagination is working
+        (don't show more than 10 products in one page).
+        """
         thumbnails = self.browser.find_elements_by_class_name('thumbnail')
         self.assertTrue(len(thumbnails) <= 10)
 
@@ -40,6 +49,10 @@ class ListProductsTest(unittest.TestCase):
 class DetailProductTest(unittest.TestCase):
 
     def setUp(self):
+        """Create new product using available endpoint
+        (/products/create_product)
+        Open detailed view page
+        """
         # create product to test on
         new_product = PRODUCT_FIXTURE
         response = requests.post(
@@ -50,7 +63,12 @@ class DetailProductTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
         self.browser.get('http://localhost:8000/products/%s' % self.product_id)
         self.product_name = PRODUCT_FIXTURE.get('name')
+
     def tearDown(self):
+        """Close browser
+        Delete created product using available endpoint
+        (/products/delete_product)
+        """
         self.browser.quit()
         requests.post(
             'http://localhost:8000/products/delete_product',
@@ -58,9 +76,14 @@ class DetailProductTest(unittest.TestCase):
         )
 
     def test_page_title(self):
+        """Confirm that product name is included in page title.
+        """
         self.assertEqual('Sport Shop - %s' % self.product_name,
-            self.browser.title)
+                         self.browser.title)
 
     def test_product_name(self):
-        product_name = self.browser.find_element_by_class_name('product-name').text
+        """Confirm that product name is shown as heading for product.
+        """
+        product_name = self.browser.find_element_by_class_name(
+            'product-name').text
         self.assertEqual(product_name, self.product_name)
