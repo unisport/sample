44735b9128a0ce7ebe57c3ce0e1b4ffc01ed8f25
Fredrik Kock <fredrik@falconsocial.com>
2015-12-27 22:38:25 +0100

    Added kids category.

--- sport_shop/products/urls.py
+++ sport_shop/products/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listing_products, name='products'),
+    url(r'kids', views.listing_products, {'product_filter': 'kids'},
+        name='products'),
     url(r'^(?P<product_id>[0-9]+)/$',
         views.detail_product, name='detail_product'),
     url(r'create_product', views.create_product, name='create_product'),
--- sport_shop/products/views.py
+++ sport_shop/products/views.py
@@ -12,10 +12,13 @@ from .models import Product
 
 
 @require_http_methods("GET")
-def listing_products(request):
+def listing_products(request, product_filter=None):
     """Lists all products sorted by price (ascending).
 
     Args: request (GET)
+          product_filter (basestring): Filtering on the given attribute
+            (must be boolean type) will be applied when getting
+            list of products.
     Request parameters:
         page (int): Page number (limited amount of products on each page)
             default is 1.
@@ -27,33 +30,39 @@ def listing_products(request):
             and navigation buttons to prev/next page if they exist.
             Page is validated and set to 1 if invalid.
     """
-    # All products ordered by price
-    all_products = Product.objects.order_by('price').all()
+    products = None
+    if product_filter:
+        q_filter = {}
+        q_filter[product_filter] = True
+        products = Product.objects.filter(**q_filter).order_by('price').all()
+    else:
+        # All products ordered by price
+        products = Product.objects.order_by('price').all()
     # Default pagination is 10 products per page
     products_per_page = request.GET.get('products_per_page', 10)
-    paginator = Paginator(all_products, products_per_page)
+    paginator = Paginator(products, products_per_page)
     template = loader.get_template('products/listing_products.html')
     page = request.GET.get('page', 1)
     try:
-        products = paginator.page(page)
+        products_page = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
         page = 1
-        products = paginator.page(page)
+        products_page = paginator.page(page)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         page = paginator.num_pages
-        products = paginator.page(page)
+        products_page = paginator.page(page)
     try:
-        prev_page = products.previous_page_number()
+        prev_page = products_page.previous_page_number()
     except EmptyPage:
         prev_page = None
     try:
-        next_page = products.next_page_number()
+        next_page = products_page.next_page_number()
     except EmptyPage:
         next_page = None
     context = {
-        'products': products,
+        'products': products_page,
         'pages_info': {
             'current': page,
             'count': paginator.num_pages,
