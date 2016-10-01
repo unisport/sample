83dde068335d27fa6cfd2358fba2792e7344a5d8
Roman Dryndik <dryndikroman@gmail.com>
2016-10-01 19:42:46 +0200

    Add Flask-Sqlalchemy and products model

--- application.py
+++ application.py
@@ -1,13 +1,16 @@
 import json
 import flask
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from schema import ProductSchema
 from helpers import DecimalJSONEncoder
 
-
 app = Flask(__name__)
 app.json_encoder = DecimalJSONEncoder
-PATH = 'data.json'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config.update(DATABASE='data.json')
+db = SQLAlchemy(app)
 
 
 def _read_data(path):
@@ -18,7 +21,7 @@ def _read_data(path):
 @app.route('/products')
 def products():
     with app.app_context():
-        data = _read_data(PATH)
+        data = _read_data(app.config['DATABASE'])
         products = ProductSchema().load(data['products'], many=True).data
         products = sorted(products, cmp=lambda item1, item2: int(item1['price'] - item2['price']))
         return flask.jsonify(products[:10])
@@ -27,7 +30,7 @@ def products():
 @app.route('/products/kids')
 def kids():
     with app.app_context():
-        data = _read_data(PATH)
+        data = _read_data(app.config['DATABASE'])
         products = ProductSchema().load(data['products'], many=True).data
         products = filter(lambda item: item['kids'] == 1, products)
         products = sorted(products, cmp=lambda item1, item2: int(item1['price'] - item2['price']))
---
+++ model/product.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from application import db
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    package = db.Column(db.Integer)
+    women = db.Column(db.Integer)
+    price = db.Column(db.Numeric(asdecimal=True), default=Decimal('0.0'))
+    price_old = db.Column(db.Numeric(asdecimal=True), default=Decimal('0.0'))
+    online = db.Column(db.Integer)
+    url = db.Column(db.String)
+    image = db.Column(db.String)
+    img_url = db.Column(db.String)
+    delivery = db.Column(db.String)
+    currency = db.Column(db.String)
+    name = db.Column(db.String)
+    sizes = db.Column(db.String)
+    kids = db.Column(db.String)
+    kid_adult = db.Column(db.Integer)
+    free_porto = db.Column(db.Integer)
