475e2c18cd92c069dc12c7895eb7b0ee0981d8bf
Roman Dryndik <dryndikroman@gmail.com>
2016-10-01 22:32:00 +0200

    Add pagination

--- application.py
+++ application.py
@@ -1,16 +1,19 @@
+from flask import request
+
 from config import app
 from model.product import Product
 from schema import ProductSchema
 
 
-@app.route('/products')
+@app.route('/products/', methods=['GET'])
 def products():
     with app.app_context():
+        page = int(request.args.get('page', 1))
         items = Product.query.order_by(Product.price).all()
-        return ProductSchema().dump(items[:10], many=True)
+        return ProductSchema().dump(items[10 * (page-1):10 * page], many=True)
 
 
-@app.route('/products/kids')
+@app.route('/products/kids', methods=['GET'])
 def kids():
     with app.app_context():
         items = Product.query.filter(Product.kids == '1').order_by(Product.price).all()
