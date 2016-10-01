94f8506deb1f554e1da7577543bcd1b2ed704707
Roman Dryndik <dryndikroman@gmail.com>
2016-10-01 20:09:52 +0200

    Fix sqlite uri

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from helpers import DecimalJSONEncoder
 
 app = Flask(__name__)
 app.json_encoder = DecimalJSONEncoder
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/products.db'
 app.config.update(DATABASE='data.json')
 db = SQLAlchemy(app)
 
