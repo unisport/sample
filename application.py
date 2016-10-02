4784d869de4e677959f64fce36c7175602704217
Roman Dryndik <dryndikroman@gmail.com>
2016-10-02 15:36:14 +0200

    WIP: add basic CRUD

--- application.py
+++ application.py
@@ -2,10 +2,9 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 import flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, redirect
 
-from config import app
+from config import app, db
 from model.product import Product
 from schema import ProductSchema, PageSchema, ValidationException, ProductIdSchema
 
@@ -13,6 +12,10 @@ PAGE_SIZE = 10
 
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/products/', methods=['GET'])
 def products():
     with app.app_context():
@@ -38,6 +41,44 @@ def product(prod_id):
         return flask.jsonify({})
     item = ProductSchema().dump(item).data
     return render_template('product.html', item=item)
+
+
+@app.route('/product/', methods=['GET', 'POST'])
+def manage_product():
+    if request.method == 'GET':
+        return render_template('create_product.html')
+    elif request.method == 'POST':
+        item = ProductSchema().load(request.form.to_dict()).data
+        db.session.add(Product(**item))
+        db.session.commit()
+        return redirect('/', code=302)
+    return redirect('/', code=405)
+
+
+@app.route('/update-product/<prod_id>/', methods=['PUT'])
+def update_product(prod_id):
+    try:
+        checked_prod_id = ProductIdSchema().load({'prod_id': prod_id}).data['prod_id']
+        item_data = ProductSchema().load(request.form.to_dict()).data
+        Product.query.filter_by(id=checked_prod_id).update(item_data)
+        db.session.commit()
+    except (ValidationException, Exception):
+        app.logger.exception('Someone tries to send non-valid product id: {}'.format(prod_id))
+        return "503"
+    return "200"
+
+
+@app.route('/product/<prod_id>/delete/', methods=['DELETE'])
+def delete_product(prod_id=None):
+    try:
+        checked_prod_id = ProductIdSchema().load({'prod_id': prod_id}).data['prod_id']
+        item = Product.query.get(checked_prod_id)
+        db.session.delete(item)
+        db.session.commit()
+    except (ValidationException, Exception):
+        app.logger.exception('Someone tries to send non-valid product id: {}'.format(prod_id))
+        return "503"
+    return redirect('/', code=200)
 
 
 @app.route('/products/kids/', methods=['GET'])
