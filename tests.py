77385beaa303be7848dd06fcaad852466a845447
Andrew <wertuk2@gmail.com>
2016-10-05 20:42:18 +0300

    Added test cases

--- tests.py
+++ tests.py
@@ -1,1 +1,26 @@
 import unittest
+from paste.fixture import TestApp
+
+from core import app
+
+
+class TestViews(unittest.TestCase):
+    def setUp(self):
+        self.testApp = TestApp(app.wsgifunc())
+
+    def test_plist(self):
+        r = self.testApp.get('/products/')
+        self.assertEqual(r.status, 200)
+
+    def test_pkids(self):
+        r = self.testApp.get('/products/kids/')
+        self.assertEqual(r.status, 200)
+
+    def test_pdetail(self):
+        r = self.testApp.get('/products/1/')
+        self.assertEqual(r.status, 200)
+        self.assertIn("u'id': u'1'", r.body)
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestViews)
+unittest.TextTestRunner(verbosity=2).run(suite)
