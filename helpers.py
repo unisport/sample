366f744689e9ef0dd4ab337092e6625b48f15c69
Roman Dryndik <dryndikroman@gmail.com>
2016-10-09 02:14:22 +0200

    Remove useless code

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import flask
-import decimal
-
-
-class DecimalJSONEncoder(flask.json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, decimal.Decimal):
-            return str(obj)
-        return super(DecimalJSONEncoder, self).default(obj)
