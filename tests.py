3ce6e44505f76fcb4aabfb62faa5d5ebe799eb60
Kristian <kn@unisport.dk>
2017-02-21 17:43:30 +0200

    just the initial stuff

--- tests.py
+++ tests.py
@@ -1,1 +1,17 @@
 import unittest
+from nose.tools import *
+
+import logging
+
+
+def setup_func():
+    pass
+
+def teardown_func():
+    pass
+
+
+@with_setup(setup_func, teardown_func)
+def test_working():
+    assert_equal("hello kitty", "hello kitty")
+
