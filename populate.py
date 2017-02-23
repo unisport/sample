ecc13455ce4fbebd4dd974d835cf1d56de1beb7b
Kristian <kn@unisport.dk>
2017-02-23 16:42:40 +0100

    More commenting

--- populate.py
+++ populate.py
@@ -5,6 +5,9 @@ import codecs
 
 
 def format_price(product):
+    """
+    Turns 0,00 into a real float
+    """
     try:
         price = product['price'].replace(',', '.')
         return float(price)
@@ -13,6 +16,9 @@ def format_price(product):
 
 
 def kids_true(product):
+    """
+    Turns kids == 1 into a boolean instead
+    """
     try:
         if product['kids'] == '1':
             return True
@@ -23,11 +29,19 @@ def kids_true(product):
 
 
 def format_id(product):
+    """
+    Turns the product id into an integer
+    """
     return int(product['id'])
 
 
 def format_name(product):
+    """
+    Ran into some weird encoding problem, tried to fix it by
+    ensuring correct encoding
+    """
     return unicode(product['name'].strip(codecs.BOM_UTF8), 'utf-8')
+
 
 with open('sample.json') as json_data:
     products = json.load(json_data)
@@ -35,4 +49,3 @@ with open('sample.json') as json_data:
     for product in products['products']:
         Product.create(name=product['name'], price=format_price(product),
             for_kids=kids_true(product), product_id=format_id(product))
-
