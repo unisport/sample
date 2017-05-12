d61bce80e77800d22600df372d1cd8a4ec0423f3
Seve <seve@openmailbox.org>
2017-05-12 17:57:48 +0200

    Add /products/ test

--- tests.py
+++ tests.py
@@ -1,1 +1,29 @@
 import unittest
+import challenge
+import json
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+
+def load_json(data):
+	return json.loads(data)
+
+class ChallengeTest(unittest.TestCase):
+	
+	
+	def setUp(self):
+		self.app = challenge.app.test_client()
+	
+	def test_products(self):
+		products = load_json(self.app.get('/products/').data)['products']
+		
+		self.assertEqual(len(products), 10)
+
+		prices = []
+		
+		for product in products:
+			prices.append(locale.atof((product['price'])))
+			
+		for i in range(1, 10):
+			self.assertGreaterEqual(prices[i], prices[i-1])
+			
