6c6e3550e31b889b5d06f5bc1d7ef235248d9532
Seve <seve@openmailbox.org>
2017-05-13 18:47:05 +0200

    Add sql schema

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS products 
+(
+	id INTEGER PRIMARY KEY,
+	is_customizable INTEGER NOT NULL,
+	delivery TEXT NOT NULL,
+	kids INTEGER NOT NULL,
+	name TEXT NOT NULL,
+	sizes TEXT NOT NULL,
+	kid_adult INTEGER NOT NULL,
+	free_porto INTEGER NOT NULL,
+	image TEXT NOT NULL,
+	package INTEGER NOT NULL,
+	price REAL NOT NULL,
+	url TEXT NOT NULL,
+	online INTEGER NOT NULL,
+	price_old REAL NOT NULL,
+	currency TEXT NOT NULL,
+	img_url TEXT NOT NULL,
+	women INTEGER NOT NULL 
+);
