9e7a7fe5d43008ae2b9049d8c4037e8344341d18
Mikkel Hansen <mikkelhansen@outlook.dk>
2017-05-23 22:49:59 +0200

    fix pep8 errors E251 in seeder.py

--- seeder.py
+++ seeder.py
@@ -39,22 +39,22 @@ def run(db):
     data = fetch_data()
     for item in data:
         product = models.Product(
-            id = int(item["id"]),
-            is_customizable = string_bit_to_boolean(item["is_customizable"]),
-            delivery = string_bit_to_boolean(item["delivery"]),
-            kids = string_bit_to_boolean(item["kids"]),
-            name = item["name"],
-            sizes = item["sizes"],
-            free_porto = string_bit_to_boolean(item["free_porto"]),
-            kid_adult = string_bit_to_boolean(item["kid_adult"]),
-            image = item["image"],
-            package = string_bit_to_boolean(item["package"]),
-            price = format_price(item["price"]),
-            url = item["url"],
-            online = string_bit_to_boolean(item["online"]),
-            price_old = string_bit_to_boolean(item["price_old"]),
-            img_url = item["img_url"],
-            women = string_bit_to_boolean(item["women"])
+            id=int(item["id"]),
+            is_customizable=string_bit_to_boolean(item["is_customizable"]),
+            delivery=string_bit_to_boolean(item["delivery"]),
+            kids=string_bit_to_boolean(item["kids"]),
+            name=item["name"],
+            sizes=item["sizes"],
+            free_porto=string_bit_to_boolean(item["free_porto"]),
+            kid_adult=string_bit_to_boolean(item["kid_adult"]),
+            image=item["image"],
+            package=string_bit_to_boolean(item["package"]),
+            price=format_price(item["price"]),
+            url=item["url"],
+            online=string_bit_to_boolean(item["online"]),
+            price_old=string_bit_to_boolean(item["price_old"]),
+            img_url=item["img_url"],
+            women=string_bit_to_boolean(item["women"])
         )
         db.session.add(product)
         db.session.commit()
