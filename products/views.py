cc3726564d5c4119708f04482ee8f62366a18e54
LasseBechJensen <lasse.bech.jensen@live.dk>
2017-05-26 01:39:38 +0200

    Messing around with Urls and Views

--- products/views.py
+++ products/views.py
@@ -1,13 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
+#from __future__ import unicode_literals
+#from django.shortcuts import render
 from django.http import HttpResponse
 
-def products(request):
-	return HttpResponse("List of products here")
-	
-def products_kids(request):
-	return HttpResponse("List of kids products here")
+def products(request, kids=None):
+	html = ""
+	if kids == None or kids == "":
+		html += "<h1>List of products here</h1>"
+	else:
+		html += "<h1>List of kids products here</h1>"
+	page = request.GET.get("page")
+	if page != None and page.isdigit():
+		html += "<br />Page " + str(page)
+	return HttpResponse(html)
 
-def product_id(rquest):
-	return HttpResponse("Individual product here")
+def product_id(request, pid=None):
+	html = ""
+	if pid == None or pid == "":
+		html += "Error you have not provided a product id"
+	else:
+		html += "Product with id: " + str(pid)
+	return HttpResponse(html)
