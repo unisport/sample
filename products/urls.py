fc6c159d19ab502909816b2275f7b53f71eac640
LasseBechJensen <lasse.bech.jensen@live.dk>
2017-05-26 12:09:46 +0200

    Fleshed out the all products and kids products views

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r"^(kids)?/?$", views.products),
-	url(r"^id/(\d*)/?$", views.product_id),
+	url(r"^$", views.products),
+	url(r"^(kids)/$", views.products),
+	url("^id/$", views.product_id),
+	url(r"^id/(\d+)/$", views.product_id),
 ]
--- products/views.py
+++ products/views.py
@@ -1,22 +1,68 @@
 #from __future__ import unicode_literals
 #from django.shortcuts import render
 from django.http import HttpResponse
+import urllib, json
 
+product_list = json.loads(urllib.urlopen("http://www.unisport.dk/api/sample/").read())["products"]
+product_list = sorted(product_list, key=lambda k: float(k["price"].replace(",", ".")))
+product_list_kids = [p for p in product_list if p["kids"] == "1"]
+
+# Views
 def products(request, kids=None):
-	html = ""
+	
 	if kids == None or kids == "":
-		html += "<h1>List of products here</h1>"
+		l = product_list
+		html = header("All products")
+		html += "<h1>All products</h1>"
+		html += "<p><a href=\"/products/kids/\">Click here to see only kids products</a></p>"
 	else:
-		html += "<h1>List of kids products here</h1>"
+		l = product_list_kids
+		html = header("Kids products")
+		html += "<h1>Kids products</h1>"
+		html += "<p><a href=\"/products/\">Click here to see all products</a></p>"
+	
+	i = 0
+	
+	html += "<table>"
+	while i < len(l):
+		p = l[i]
+		html += "<tr>"
+		html += "<td><a href=\"/products/id/%s\">%s</a></td>" % (p["id"], p["name"])
+		html += "<td align=\"right\">%s</td>" % p["price"]
+		html += "<td>%s</td>" % p["currency"]
+		html += "</tr>"
+		i += 1
+	html += "</table>"
+	
 	page = request.GET.get("page")
 	if page != None and page.isdigit():
 		html += "<br />Page " + str(page)
+	
+	html += footer()
 	return HttpResponse(html)
 
 def product_id(request, pid=None):
-	html = ""
+
 	if pid == None or pid == "":
-		html += "Error you have not provided a product id"
+		html = header("No product id provided")
+		html += "<p>You have not provided a product id</p>"
+		html += "<p><a href=\"/products/\">Back to product list</a></p>"
 	else:
-		html += "Product with id: " + str(pid)
+		product = next((p for p in product_list if p["id"] == pid), None)
+		html = header(product["name"])
+		html += "<p><a href=\"/products/\">Back to product list</a></p>"
+		html += "<h1>%s</h1>" % product["name"]
+	
+	html += footer()
 	return HttpResponse(html)
+
+# HTML Helper functions
+def header(title):
+	html = "<html><head><title>"
+	html += title
+	html += "</title></head><body>"
+	return html
+
+def footer():
+	html = "</body></html>"
+	return html
