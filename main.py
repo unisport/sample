bf4732b4d4d5486f653e87968f2e67c14456b41c
Niels Lerche Sørensen <nielslerches@gmail.com>
2017-09-14 14:28:54 +0200

    Commented and renamed price-parsing function to parse_money.

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ from re import match
 
 app = Flask(__name__)
 
-def parse_price(s, c):
+"""
+Parses a money-amount based on its value and currency-type.
+
+:param s: the value of the money
+:param c: the currency-type of the money
+:returns: Money(s, c)
+"""
+def parse_money(s, c):
     if match(".+(\.|,)00", s):
         return Money(s[:-3], c)
     return Money(s, c)
@@ -13,14 +20,14 @@ def parse_price(s, c):
 @app.route("/products")
 def products():
     products_list = data["products"]
-    cheapest_products = sorted(products_list, key=lambda p: parse_price(p["price"], p["currency"]))[:10]
+    cheapest_products = sorted(products_list, key=lambda p: parse_money(p["price"], p["currency"]))[:10]
 
     return jsonify(cheapest_products)
 
 @app.route("/products/kids")
 def kids_products():
     kids_products_list = [p for p in data["products"] if p["kid_adult"] == "1"]
-    cheapest_kids_products = sorted(kids_products_list, key=lambda p: parse_price(p["price"], p["currency"]))
+    cheapest_kids_products = sorted(kids_products_list, key=lambda p: parse_money(p["price"], p["currency"]))
 
     return jsonify(cheapest_kids_products)
 
