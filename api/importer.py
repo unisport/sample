7b4cbbf0524f795bbb3cdf2ff5942ed97e0bfcd5
stefano <s.misuraca@globalscanning.com>
2020-02-28 14:16:33 +0100

    remove zombie code and minor changes

--- api/importer.py
+++ api/importer.py
@@ -34,7 +34,6 @@ def import_unisport_data():
                 product_serializer.is_valid()
             except AssertionError as e:
                 logger.info(product_serializer.errors)
-                raise AssertionError(e)
             try:
                 product_serializer.save()
                 logger.info(f"product {product.get('id')} imported")
