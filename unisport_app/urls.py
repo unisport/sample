6472ce664267a65d102f7212a881330880fec88e
Linux User <jonas@kea-dev.members.linode.com>
2022-07-29 15:39:15 +0200

    urls added - products_db, products_db/id

--- unisport_app/urls.py
+++ unisport_app/urls.py
@@ -5,7 +5,15 @@ from . import views
 app_name = 'unisport_app'
 
 urlpatterns = [
+    # Index - redirects to products/
     path('', views.index, name='index'),
+
+    # views using API data
     path('products/', views.products, name='products'),
     path('products/<int:id>/', views.product_detail, name='product_detail'),
+
+    # views using data from database
+    path('products_db/', views.products_from_db, name='products_from_db'),
+    path('products_db/<int:id>/', views.product_from_db_detail,
+         name='product_from_db_detail'),
 ]
