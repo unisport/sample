67025a4aacbe4d7cd74b9e2c5a17e0a0c9245060
Linux User <jonas@kea-dev.members.linode.com>
2022-07-29 02:54:31 +0200

    setting up database models

--- unisport_app/models.py
+++ unisport_app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    unisport_id = models.CharField(max_length=6)
+    name = models.TextField()  # Or VARCHAR?
+    relative_url = models.CharField(max_length=255)
+    image = models.URLField()
+    delivery = models.CharField(max_length=255)
+    online = models.BooleanField(default=False)
+    is_customizable = models.BooleanField(default=False)
+    is_exclusive = models.BooleanField(default=False)
+    url = models.URLField()
+
+    @property
+    def stock(self):
+        return Stock.objects.filter(product_id=self.pk)
+
+    @property
+    def price(self):
+        return Price.objects.filter(product_id=self.pk)
+
+    def __str__(self):
+        return f'{self.pk}/{self.unisport_id} - {self.name}'
+
+
+class Stock(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50)  # Name from API?
+    stock_quantity = models.IntegerField()  # Additional
+    is_marketplace = models.BooleanField()
+    name_short = models.CharField(max_length=30)
+    # order_by = models.IntegerField() # From API
+    # pk = models.IntegerField() # From API
+
+    # Create unique constraint on product + size
+    class Meta:
+        db_table = 'stock'
+        constraints = [models.UniqueConstraint(
+            fields=['product_id', 'size'], name='unique product size')]
+
+    def __str__(self):
+        return f'{self.product_id} - {self.size} - {self.stock_quantity}'
+
+
+class Currency(models.Model):
+    currency_code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f'{self.currency_code}'
+
+
+class Price(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    max_price = models.DecimalField(max_digits=8, decimal_places=2)
+    min_price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_percentage = models.IntegerField()
+    recommended_retail_price = models.DecimalField(
+        max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.product_id} - {self.currency}: {self.max_price}, Discount: {self.discount_percentage}%'
