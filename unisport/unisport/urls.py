902168c1e7e9c8683f663d3af0d75a0852949dbe
MadsHaderup <mads@haderup.net>
2022-08-16 15:14:45 +0200

    complete CRUD implemented

--- unisport/unisport/urls.py
+++ unisport/unisport/urls.py
@@ -20,5 +20,5 @@ from unisport_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', views.ApiProductListView.as_view(),),
-    path('products/<int:id>/', views.ApiProductView.as_view()),
+    path('products/<int:id>/', views.ApiProductView.as_view(),),
 ]
--- unisport/unisport_app/views.py
+++ unisport/unisport_app/views.py
@@ -7,7 +7,10 @@ from rest_framework.generics import ListAPIView
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
+
+#Post and list all in same class because they are not id spefic 
 class ApiProductListView(ListAPIView):
     queryset = Product.objects.all().order_by("prices__recommended_retail_price")
     serializer_class = ProductSerializer
@@ -15,8 +18,25 @@ class ApiProductListView(ListAPIView):
     permission_classes = (IsAuthenticated,)
     pagination_class = PageNumberPagination
 
+    def post(self, request):
+        data = request.data
+        serializer = ProductSerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
+        response = Response()
+
+        response.data = {
+            'message': 'Product Created Successfully',
+            'data': serializer.data
+        }
+
+        return response
+
+
 class ApiProductView(APIView):
     permission_classes = (IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+    pagination_class = PageNumberPagination
 
     def get_product(self, id):
 
@@ -29,10 +49,41 @@ class ApiProductView(APIView):
         
         product = self.get_product(id)
         if not product:
-            return JsonResponse(
-                {"res": "Object with id does not exists"},
+            return Response(
+                {"res": "Product with this id does not exists"},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
         serializer = ProductSerializer(product)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        
+        product = self.get_product(id)
+        if not product:
+            return Response(
+                {"res": "Product with this id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer = ProductSerializer(instance=product, data=request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        
+        product = self.get_product(id)
+        if not product:
+            return Response(
+                {"res": "Product with this id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        product.delete()
+        return Response(
+            {"res": "Product deleted!"},
+            status=status.HTTP_200_OK
+        )
+
+
